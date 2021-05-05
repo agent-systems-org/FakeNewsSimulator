ee493aef9eeb4e61b94a19d7728fb02813228d73
rachwalk <kajetan.rachwal@mailfence.com>
2021-05-05 20:07:45 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from spade import agent
+
+class DummyAgent(agent.Agent):
+    async def setup(self):
+        print("Hello World! I'm agent {}".format(str(self.jid)))
+
+dummy = DummyAgent("test_agent@jabbim.pl", "123")
+future = dummy.start()
+future.result()
+
+dummy.stop()
