edbf513ce9522edcced3e8d82cb177b0ac58df0f
madpeh <48161600+madpeh@users.noreply.github.com>
2021-05-12 00:47:25 +0200

    Format with black

--- agents/dummy.py
+++ agents/dummy.py
@@ -4,14 +4,26 @@ from spade.behaviour import OneShotBehaviour
 
 
 class DummyAgent(Agent):
-    def __init__(self, graph_creator_jid, jid, password, location, adj_list, verify_security=False):
+    def __init__(
+        self,
+        graph_creator_jid,
+        jid,
+        password,
+        location,
+        adj_list,
+        verify_security=False,
+    ):
         super().__init__(jid, password, verify_security)
         self.location = location
         self.adj_list = adj_list
         self.graph_creator_jid = graph_creator_jid
 
     async def setup(self):
-        print("Hello World! I'm agent {} my location is {}".format(str(self.jid), str(self.location)))
+        print(
+            "Hello World! I'm agent {} my location is {}".format(
+                str(self.jid), str(self.location)
+            )
+        )
         print("My neighbours {}".format(self.adj_list))
         print("I was created by {}".format(self.graph_creator_jid))
         b = self.getNeighbours(self.graph_creator_jid)
@@ -21,13 +33,12 @@ class DummyAgent(Agent):
         def __init__(self, graph_creator_jid):
             super().__init__()
             self.graph_creator_jid = graph_creator_jid
+
         async def run(self):
             msg = Message(to=str(self.graph_creator_jid))
             msg.set_metadata("performative", "query")
             await self.send(msg)
 
-            msg = await self.receive(timeout = 10)
+            msg = await self.receive(timeout=10)
             if msg:
                 print(msg.body)
-
-        
