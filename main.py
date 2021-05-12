5215d228ca1689a4d3c68fb6a44798642627feae
madpeh <48161600+madpeh@users.noreply.github.com>
2021-05-12 19:56:11 +0200

    Run visualization in main loop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from  agents.graphcreator import GraphCreator
+from agents.graphcreator import GraphCreator
 from visualization import visualize_network
 import time
+import matplotlib.pyplot as plt
+
 
 def main():
     jid = "test_agent@jabbim.pl/21360"
@@ -16,12 +18,13 @@ def main():
         future = agent.start()
         future.result()
 
-    visualize_network(g.agents)
-
-    all_alive = lambda: all(map(lambda agent: agent.is_alive(), [agent for agent in agents]))
+    all_alive = lambda: all(
+        map(lambda agent: agent.is_alive(), [agent for agent in agents])
+    )
 
     while g.is_alive() and all_alive():
         try:
+            visualize_network(g.agents, pause_time_sec=5)
             time.sleep(5)
         except KeyboardInterrupt:
             for agent in agents:
@@ -29,6 +32,7 @@ def main():
 
             g.stop()
             break
+
 
 if __name__ == "__main__":
     main()
