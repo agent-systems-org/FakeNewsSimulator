70adf8c7a39158868147de506cf3f1cbc7753bcf
madpeh <48161600+madpeh@users.noreply.github.com>
2021-05-12 22:33:25 +0200

    Fix starting agent

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 from agents.graphcreator import GraphCreator
+from spade import quit_spade
 from visualization import visualize_network
 import time
-import matplotlib.pyplot as plt
+import concurrent
+import sys
+
+
+AGENTS_DEFAULT_COUNT = 16
 
 
 def main():
-    jid = "test_agent@jabbim.pl/21360"
+    if len(sys.argv) == 2:
+        agents_count = int(sys.argv[1])
+    else:
+        agents_count = AGENTS_DEFAULT_COUNT
 
-    g = GraphCreator(jid, "123", 8)
+    print(f"Creating network with {agents_count} agents")
+
+    jid = "test_agent@jabbim.pl/2000000"
+
+    g = GraphCreator(jid, "123", agents_count)
 
     future = g.start()
     future.result()
 
     agents = g.agents
 
-    for agent in agents:
-        future = agent.start()
-        future.result()
+    done, not_done = concurrent.futures.wait(
+        [agent.start() for agent in agents],
+        timeout=agents_count / 2,
+        return_when=concurrent.futures.ALL_COMPLETED,
+    )
+    time.sleep(1)
+    print(f"created: {len(done)}, failed: {len(not_done)}")
 
     all_alive = lambda: all(
         map(lambda agent: agent.is_alive(), [agent for agent in agents])
@@ -32,6 +48,8 @@ def main():
 
             g.stop()
             break
+
+    quit_spade()
 
 
 if __name__ == "__main__":
