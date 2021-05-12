c03724e8edb0f6cbf000b6e271e49c7efec5bc63
madpeh <48161600+madpeh@users.noreply.github.com>
2021-05-12 22:46:13 +0200

    Refactor

--- main.py
+++ main.py
@@ -17,12 +17,11 @@ def main():
 
     print(f"Creating network with {agents_count} agents")
 
-    jid = "test_agent@jabbim.pl/2000000"
-
-    graph_creator = GraphCreator(jid, "123", agents_count)
-
-    future = graph_creator.start()
-    future.result()
+    first_jid = "test_agent@jabbim.pl/3000000"
+    password = "123"
+    
+    graph_creator = GraphCreator(first_jid, password, agents_count)
+    graph_creator.start().result()
 
     agents = graph_creator.agents
 
@@ -34,9 +33,11 @@ def main():
     time.sleep(1)
     print(f"Created: {len(done)}, failed: {len(not_done)}")
 
-    all_alive = lambda: all(map(lambda agent: agent.is_alive(), agents))
+    are_all_agents_alive = lambda: all(
+        map(lambda agent: agent.is_alive(), agents + [graph_creator])
+    )
 
-    while graph_creator.is_alive() and all_alive():
+    while are_all_agents_alive():
         try:
             visualize_network(graph_creator.agents, pause_time_sec=5)
             time.sleep(5)
