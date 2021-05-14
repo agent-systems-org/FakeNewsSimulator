c17741a59f14d3882265a8104998698a9b9b7e4f
madpeh <48161600+madpeh@users.noreply.github.com>
2021-05-14 20:31:10 +0200

    Add behaviour with sending bot to visualization

--- agents/dummy.py
+++ agents/dummy.py
@@ -2,8 +2,8 @@ import datetime
 import json
 from spade.agent import Agent
 from spade.message import Message
-from spade.behaviour import OneShotBehaviour
-from visualization import post_message
+from spade.behaviour import OneShotBehaviour, PeriodicBehaviour
+from visualization import post_agent
 
 
 class DummyAgent(Agent):
@@ -30,19 +30,18 @@ class DummyAgent(Agent):
         self.log(
             f"dummy, location: {self.location}, neighbours: {self.adj_list}, created by: {self.graph_creator_jid}"
         )
-        b = self.GetNeighboursBehaviour(self.graph_creator_jid)
-        self.add_behaviour(b)
+
+        get_neighbours_behaviour = self.GetNeighboursBehaviour()
+        self.add_behaviour(get_neighbours_behaviour)
+
+        send_self_to_visualization = self.SendSelfToVisualization(
+            period=10, start_at=datetime.datetime.now()
+        )
+        self.add_behaviour(send_self_to_visualization)
 
     class GetNeighboursBehaviour(OneShotBehaviour):
-        def __init__(self, graph_creator_jid):
-            super().__init__()
-            self.graph_creator_jid = graph_creator_jid
-
         async def run(self):
-            # TODO test message to be removed
-            post_message(self.agent.jid, self.agent.graph_creator_jid, "query")
-
-            msg = Message(to=str(self.graph_creator_jid))
+            msg = Message(to=str(self.agent.graph_creator_jid))
             msg.set_metadata("performative", "query")
             await self.send(msg)
 
@@ -56,3 +55,7 @@ class DummyAgent(Agent):
                 self.agent.log(
                     f"querying graph creator resulted in either timeout or empty message"
                 )
+
+    class SendSelfToVisualization(PeriodicBehaviour):
+        async def run(self):
+            post_agent(self.agent)
