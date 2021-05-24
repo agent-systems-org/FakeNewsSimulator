6e5ba56d5334f7b579ed75c2b5521cdc84832b6e
madpeh <48161600+madpeh@users.noreply.github.com>
2021-05-24 23:59:41 +0200

    Refactor bot test

--- test_bots.py
+++ test_bots.py
@@ -1,29 +1,115 @@
 import time
+import random
+import concurrent
+import math
+import sys
+import getopt
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from sklearn.neighbors import KDTree
+from spade import quit_spade
 from agents import Bot
+from visualization import visualize_connections
 
-BOT_1_JID = "test_agent@jabbim.pl/111"
-BOT_2_JID = "test_agent@jabbim.pl/222"
-BOT_3_JID = "test_agent@jabbim.pl/333"
+
+ADDRESS = "test_agent@jabbim.pl/"
+PASSWORD = "123"
+FIRST_ID = 1000
+MAX_COORD = 100
+DIMENSIONS = 2
+DEFAULT_NUM_BOTS = 32
+DEFAULT_NUM_MSGS = 20
+DEFAULT_IS_CONNECTIONS_VISUALIZATION_ON = False
+
+
+def parse_cli_args():
+    usage_str = f"Usage: {sys.argv[0]} [-h] [-n <num_of_bots> (default={DEFAULT_NUM_BOTS})] [-m <num_of_msgs> (default={DEFAULT_NUM_MSGS})] [-v (run visualization of connections, default={DEFAULT_IS_CONNECTIONS_VISUALIZATION_ON})]"
+
+    try:
+        opts, _ = getopt.getopt(sys.argv[1:], "hvn:m:")
+    except getopt.GetoptError:
+        print(usage_str)
+        sys.exit(2)
+
+    num_bots = DEFAULT_NUM_BOTS
+    num_msgs = DEFAULT_NUM_MSGS
+    is_connections_visualization_on = DEFAULT_IS_CONNECTIONS_VISUALIZATION_ON
+
+    for opt, arg in opts:
+        if opt == "-h":
+            print(usage_str)
+            sys.exit(0)
+        elif opt == "-n":
+            num_bots = int(arg)
+        elif opt == "-m":
+            num_msgs = int(arg)
+        elif opt == "-v":
+            is_connections_visualization_on = (
+                not DEFAULT_IS_CONNECTIONS_VISUALIZATION_ON
+            )
+
+    return num_bots, num_msgs, is_connections_visualization_on
 
 
 if __name__ == "__main__":
-    bot1 = Bot(BOT_1_JID, "123", (1, 1), [BOT_2_JID, BOT_3_JID])
-    bot1.fakenews_msgs = ["msg1", "msg2"]
-    bot1.start()
+    NUM_BOTS, NUM_MSGS, IS_CONNECTIONS_VISUALIZATION_ON = parse_cli_args()
 
-    bot2 = Bot(BOT_2_JID, "123", (30, 30), [BOT_1_JID, BOT_3_JID])
-    bot2.fakenews_msgs = ["msg3"]
-    bot2.start()
+    bots_addresses = [f"{ADDRESS}{FIRST_ID + i}" for i in range(NUM_BOTS)]
+    msgs = [f"msg{i}" for i in range(NUM_MSGS)]
 
-    bot3 = Bot(BOT_3_JID, "123", (20, 70), [BOT_1_JID, BOT_2_JID])
-    bot3.start()
+    # generate uniformly distributed coordinates
+    X = np.random.random((NUM_BOTS, DIMENSIONS)) * MAX_COORD
 
-    while True:
-        try:
-            time.sleep(1)
-        except KeyboardInterrupt:
-            break
+    # change type to list of tuples
+    coordinates = list(map(tuple, X))
+    coordinates = [(round(x), round(y)) for x, y in coordinates]
+    kd_tree = KDTree(coordinates)
 
-    bot1.stop()
-    bot2.stop()
-    bot3.stop()
+    bots = []
+    for i in range(NUM_BOTS):
+        location = coordinates[i]
+
+        # uniformly distributed
+        # a bot cannot be a neighbour with itself
+        num_neigbours = random.randint(0, NUM_BOTS - 1)
+
+        # +1 becuase it also returns current location
+        _, nearest_indices = kd_tree.query([location], k=num_neigbours + 1)
+        neighbours_indices = nearest_indices[0][1:]
+        neighbours = [bots_addresses[idx] for idx in neighbours_indices]
+
+        bot = Bot(bots_addresses[i], PASSWORD, location, neighbours)
+
+        num_fakenews_msgs = random.randint(0, NUM_MSGS)
+        fakenews_msgs = random.sample(msgs, num_fakenews_msgs)
+        bot.fakenews_msgs = fakenews_msgs
+
+        bots.append(bot)
+
+    with concurrent.futures.ThreadPoolExecutor() as e:
+        e.submit([bot.start() for bot in bots])
+
+    if IS_CONNECTIONS_VISUALIZATION_ON:
+        fig = plt.figure()
+
+        _ = animation.FuncAnimation(
+            fig,
+            visualize_connections,
+            fargs=(bots,),
+            interval=math.sqrt(len(bots)) * 1000,
+        )
+
+        plt.show()
+
+    else:
+        while True:
+            try:
+                time.sleep(1)
+            except KeyboardInterrupt:
+                break
+
+    for bot in bots:
+        bot.stop()
+
+    quit_spade()
