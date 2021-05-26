9dd5ded9d21b05fea4bcebe3a4192b97c0b07c1b
madpeh <48161600+madpeh@users.noreply.github.com>
2021-05-27 01:11:05 +0200

    Add cli to main

--- main.py
+++ main.py
@@ -1,38 +1,78 @@
 import concurrent
 import time
 import sys
+import getopt
+import math
 from spade import quit_spade
 from agents import GraphCreator
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from visualization import visualize_connections
 
 
-AGENTS_DEFAULT_COUNT = 8
+FIRST_JID = "test_agent@jabbim.pl/10000"
+PASSWORD = "123"
+DEFAULT_NUM_AGENTS = 16
+DEFAULT_IS_CONNECTIONS_VISUALIZATION_ON = False
+
+
+def parse_cli_args():
+    usage_str = f"Usage: {sys.argv[0]} [-h] [-n <num_of_agents> (default={DEFAULT_NUM_AGENTS})] [-v (toggle visualization of connections, default={DEFAULT_IS_CONNECTIONS_VISUALIZATION_ON})]"
+
+    try:
+        opts, _ = getopt.getopt(sys.argv[1:], "hvn:")
+    except getopt.GetoptError:
+        print(usage_str)
+        sys.exit(2)
+
+    num_agents = DEFAULT_NUM_AGENTS
+    is_connections_visualization_on = DEFAULT_IS_CONNECTIONS_VISUALIZATION_ON
+
+    for opt, arg in opts:
+        if opt == "-h":
+            print(usage_str)
+            sys.exit(0)
+        elif opt == "-n":
+            num_agents = int(arg)
+        elif opt == "-v":
+            is_connections_visualization_on = (
+                not DEFAULT_IS_CONNECTIONS_VISUALIZATION_ON
+            )
+
+    return num_agents, is_connections_visualization_on
 
 
 def main():
-    if len(sys.argv) == 2:
-        agents_count = int(sys.argv[1])
-    else:
-        agents_count = AGENTS_DEFAULT_COUNT
+    NUM_AGENTS, IS_CONNECTIONS_VISUALIZATION_ON = parse_cli_args()
 
-    print(f"Creating network with {agents_count} agents")
+    print(f"Creating network with {NUM_AGENTS} agents")
 
-    first_jid = "test_agent@jabbim.pl/3000000"
-    password = "123"
-
-    graph_creator = GraphCreator(first_jid, password, agents_count)
+    graph_creator = GraphCreator(FIRST_JID, PASSWORD, NUM_AGENTS)
     graph_creator.start().result()
 
     agents = graph_creator.agents
 
-    # by default it uses at most 32 CPU cores
     with concurrent.futures.ThreadPoolExecutor() as e:
         e.submit([agent.start() for agent in agents])
 
-    while True:
-        try:
-            time.sleep(10)
-        except KeyboardInterrupt:
-            break
+    if IS_CONNECTIONS_VISUALIZATION_ON:
+        fig = plt.figure()
+
+        _ = animation.FuncAnimation(
+            fig,
+            visualize_connections,
+            fargs=(agents,),
+            interval=math.sqrt(len(agents)) * 1000,
+        )
+
+        plt.show()
+
+    else:
+        while True:
+            try:
+                time.sleep(10)
+            except KeyboardInterrupt:
+                break
 
 
 if __name__ == "__main__":
