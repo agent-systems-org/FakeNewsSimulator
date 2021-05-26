3ec982a4a75c5cc352f96bc49d9b1b03c44e8b86
madpeh <48161600+madpeh@users.noreply.github.com>
2021-05-27 00:15:45 +0200

    Fix

--- agents/common.py
+++ agents/common.py
@@ -5,12 +5,14 @@ from agents.utils import Message as News
 from spade.behaviour import PeriodicBehaviour, CyclicBehaviour
 from spade.agent import Agent
 from spade.message import Message
+from visualization import post_agent, post_messages
 
-INIT_DISPOSITION = 50  # TBD
-MAX_RECEIVE_TIME_SEC = 2137
+INIT_SUSCEPTIBILITY = 50  # TBD
+MAX_RECEIVE_TIME_SEC = 1000
 MAX_INITIAL_DELAY_SEC = 30
 MAX_SPREAD_INTERVAL_SEC = 120
-CONV_C = 16  # convergance constant
+CONVERGENCE = 16
+SEND_SELF_PERIOD_SEC = 10
 
 
 class Common(Agent):
@@ -23,32 +25,42 @@ class Common(Agent):
         self.beliving = []
         self.debunking = []
         self.graph_creator_jid = graph_creator_jid
-        self.disposition = INIT_DISPOSITION
-        self.topic = topic
+        self.susceptibility = INIT_SUSCEPTIBILITY
+        self.susceptible_topic = topic
         self.period_debunk = random.randint(3, MAX_SPREAD_INTERVAL_SEC)
         self.period_share = random.randint(3, MAX_SPREAD_INTERVAL_SEC)
         self.delay = random.randint(1, MAX_INITIAL_DELAY_SEC)
+        self.type = "common"
 
     def log(self, msg):
-        full_date = datetime.datetime.now
+        full_date = datetime.datetime.now()
         time = datetime.datetime.strftime(full_date, "%H:%M:%S")
         print(f"[{time}] {str(self.jid)}: {msg}")
 
     async def setup(self):
         self.log(
-            f"common, location: {self.location}, neighbours: {self.adj_list}, topic: {self.topic}"
+            f"common, location: {self.location}, neighbours: {self.adj_list}, susceptible topic: {self.susceptible_topic}"
         )
-        start_at = datetime.datetime.now() + datetime.timedelta(second=self.delay)
+
         self.accept_news_behaviour = self.AcceptNews()
         self.add_behaviour(self.accept_news_behaviour)
+
+        start_at = datetime.datetime.now() + datetime.timedelta(seconds=self.delay)
         self.share_news_behaviour = self.ShareNews(
             period=self.period_share, start_at=start_at
         )
         self.add_behaviour(self.share_news_behaviour)
-        start_at = datetime.datetime.now() + datetime.timedelta(second=self.delay)
+
+        start_at = datetime.datetime.now() + datetime.timedelta(seconds=self.delay)
         self.debunk_behaviour = self.ShareDebunk(
             period=self.period_debunk, start_at=start_at
         )
+        self.add_behaviour(self.debunk_behaviour)
+
+        self.send_self_to_visualization = self.SendSelfToVisualization(
+            period=SEND_SELF_PERIOD_SEC, start_at=datetime.datetime.now()
+        )
+        self.add_behaviour(self.send_self_to_visualization)
 
     class AcceptNews(CyclicBehaviour):
         async def run(self):
@@ -70,16 +82,23 @@ class Common(Agent):
                     content not in self.agent.debunking
                 ):
                     # its math time
-                    M = content.calculate_power()
-                    E = 1 / (1 + 10 ** ((M - self.agent.disposition) / 50))
+                    msg_power = content.calculate_power()
+                    expected_score = 1 / (
+                        1 + 10 ** ((msg_power - self.agent.susceptibility) / 50)
+                    )
                     result = random.uniform(0, 100)
-                    if result > 50 - (M - self.agent.disposition):  # accept the msg
-                        self.agent.disposition = self.agent.disposition + CONV_C * (
-                            1 - E
+                    if result > 50 - (
+                        msg_power - self.agent.susceptibility
+                    ):  # accept the msg
+                        self.agent.susceptibility = (
+                            self.agent.susceptibility
+                            + CONVERGENCE * (1 - expected_score)
                         )
                         self.agent.beliving.append(content)
                     else:  # refute the msg
-                        self.agent.disposition = self.agent.disposition + CONV_C * (-E)
+                        self.agent.susceptibility = (
+                            self.agent.susceptibility + CONVERGENCE * (-expected_score)
+                        )
                         self.agent.debunking.append(content)
 
     class ShareNews(PeriodicBehaviour):
@@ -130,3 +149,7 @@ class Common(Agent):
                 self.agent.log(
                     f"couldn't spread debunk, reason: neighbours: {self.agent.adj_list}, fakenews: {self.agent.fakenews_msgs}"
                 )
+
+    class SendSelfToVisualization(PeriodicBehaviour):
+        async def run(self):
+            post_agent(self.agent)
